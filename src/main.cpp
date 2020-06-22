8c75b51d0352bb6ca38f05b671f206cf155aed5f
BogdanSav <bohdan16@live.com>
2020-06-23 00:23:24 +0300

    vert axis added

--- src/main.cpp
+++ src/main.cpp
@@ -4,37 +4,39 @@
 
 #define Xpin A0
 #define Ypin A1
-int angleX = 0;
-int angleY = 0;
+#define ButtonPin 6
+int angleX = 90;
+int angleY = 90;
+int angleY1 = 90;
+int count = 0;
+
 Joystick joy(Xpin, Ypin);
-Servo servo1;
-Servo servo2;
+Servo horizServo;
+Servo vertServo1;
+Servo vertServo2;
 
 void setup()
 {
   Serial.begin(9600);
   joy.begin();
-  servo1.attach(9);
-  servo2.attach(11);
-  
-  // angleX = servo1.read();
-  // angleY = servo2.read();
-
-  // servo1.write(angleX);
-  // servo2.write(angleY);
+  pinMode(ButtonPin, INPUT);
+  horizServo.attach(9);
+  vertServo1.attach(11);
+  vertServo2.attach(10);
 }
 
 void loop()
 {
-  Serial.println("y:");
-  Serial.println(joy.getValueY());
-  Serial.println("x:");
-  Serial.println(joy.getValueX());
+  // Serial.println("y:");
+  // Serial.println(joy.getValueY());
+  // Serial.println("x:");
+  // Serial.println(joy.getValueX());
+
   // first servo
   if (joy.getValueX() > 503)
   {
     angleX += joy.positiveDirectonX();
-    servo1.write(angleX);
+    horizServo.write(angleX);
 
     if (angleX > 180)
     {
@@ -46,29 +48,93 @@ void loop()
   else if (joy.getValueX() < 483)
   {
     angleX -= joy.negativeDirectonX();
-    servo1.write(angleX);
+    horizServo.write(angleX);
 
     if (angleX < 0)
     {
       angleX = 0;
     }
   }
+
+  if (digitalRead(ButtonPin) == LOW)
+  {
+    delay(300);
+    count++;
+  }
+
   // second servo
   if (joy.getValueY() > 530)
   {
-    angleY += joy.positiveDirectonY();
-    servo2.write(angleY);
+
+    switch (count)
+    {
+    case 0:
+      angleY = vertServo1.read();
+      angleY1 = vertServo2.read();
+
+      angleY += joy.positiveDirectonY();
+      angleY1 += joy.positiveDirectonY();
+
+      vertServo1.write(angleY);
+      vertServo2.write(angleY1);
+      break;
+    case 1:
+      angleY = vertServo1.read();
+      angleY += joy.positiveDirectonY();
+      vertServo1.write(angleY);
+      break;
+    case 2:
+      angleY1 = vertServo2.read();
+      angleY1 += joy.positiveDirectonY();
+      vertServo2.write(angleY1);
+      break;
+    }
+
+    if (count > 2)
+    {
+      count = 0;
+    }
+
     if (angleY > 180)
     {
       angleY = 180;
     }
+
     delay(20);
   }
   else if (joy.getValueY() < 510)
   {
 
-    angleY -= joy.negativeDirectonY();
-    servo2.write(angleY);
+    // angleY -= joy.negativeDirectonY();
+
+    switch (count)
+    {
+    case 0:
+      angleY = vertServo1.read();
+      angleY1 = vertServo2.read();
+
+      angleY -= joy.negativeDirectonY();
+      angleY1 -= joy.negativeDirectonY();
+
+      vertServo1.write(angleY);
+      vertServo2.write(angleY1);
+      break;
+    case 1:
+      angleY = vertServo1.read();
+      angleY -= joy.negativeDirectonY();
+      vertServo1.write(angleY);
+      break;
+    case 2:
+      angleY1 = vertServo2.read();
+      angleY1 -= joy.negativeDirectonY();
+      vertServo2.write(angleY1);
+      break;
+    }
+
+    if (count > 2)
+    {
+      count = 0;
+    }
 
     if (angleY < 0)
     {
